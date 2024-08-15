c370b4e8f176042f7be294e3e252bf2a33060f2e
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-08-15 19:02:51 +0200

    Feat: added needed order api call routes

--- routes/api/v1/OrderRoutes.js
+++ routes/api/v1/OrderRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const OrderController = require('../../../controllers/api/v1/OrderController');
 
+// Post a new entry to order file
+router.post('/', OrderController.createOrder);
+
+// GET all Orders
+router.get('/', OrderController.getAllOrders);
+
+// Route to update order status
+router.put('/:id/status', OrderController.updateOrderStatus);
+
+// Get order count
+router.get('/count', OrderController.getOrderCount);
 
 module.exports = router;
